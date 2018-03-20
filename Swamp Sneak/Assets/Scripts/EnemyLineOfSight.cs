481a129a4d79e8e1938c28af5a2c28aae540c06d
Anthony Rossello <anthony@Mithrandir.local>
2018-03-20 03:21:48 -0400

    Implemented line of sight

---
+++ Swamp Sneak/Assets/Scripts/EnemyLineOfSight.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLineOfSight : MonoBehaviour {
+
+	public Transform playerTransform;
+	public float degreeOfSight;
+	float roty = 20;
+
+	bool PlayerIsSeenByEnemy() {
+		Vector3 playerDir = playerTransform.position - transform.position;
+		float angle = Vector3.Angle (playerDir, transform.forward);
+
+		if (angle <= degreeOfSight) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
+
+	void Update() {
+		if(PlayerIsSeenByEnemy()) {
+			Debug.Log ("Seen!");
+		}
+		else {
+			Debug.Log ("Not seen.");
+		}
+		if (Time.deltaTime%30<=15)
+			transform.Rotate (0, roty * Time.deltaTime, 0);
+		else
+			transform.Rotate (0, -roty * Time.deltaTime, 0);
+	
+	}
+}
