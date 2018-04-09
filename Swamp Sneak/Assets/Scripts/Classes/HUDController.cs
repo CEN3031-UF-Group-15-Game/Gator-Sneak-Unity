0642c32326c7a7fdae40a0cd0cf051a69e25eb3a
Jazy Llerena <jazy@jazyserver.com>
2018-04-09 01:55:57 -0400

    Implement basic HUD method hooks
    Implement HUD public update methods for the Score, Timer, and Gamemode
    
    Also create a very basic debug method for testing the HUD update methods

--- Swamp Sneak/Assets/Scripts/Classes/HUDController.cs
+++ Swamp Sneak/Assets/Scripts/Classes/HUDController.cs
@@ -1,13 +1,34 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class HUDController : MonoBehaviour {
 	
 	public KeyCode HideHotkey;
+	public bool DebugMode;
 	
     private Canvas CanvasObject;
+	private Text ScoreText;
+	private Text TimerText;
+	private Text GamemodeText;
 
+	public void updateScoreValue(int score) {
+		ScoreText.text = score.ToString();
+	}
+	public void updateTimerSecondsRemaining(int seconds) {
+		int min = (int)(seconds / 60);
+		int sec = seconds % 60;
+		updateTimerValue(min.ToString() + ":" + sec.ToString("00"));
+	}
+	public void updateGamemodeString(string gamemodeStr) {
+		GamemodeText.text = gamemodeStr;
+	}
+
+	// Will set the timer value to (time) exactly
+	public void updateTimerValue(string time) {
+		TimerText.text = time;
+	}
 	// Hide the canvas
 	public void hide() {
 		CanvasObject.enabled = false;
@@ -23,7 +44,11 @@ public class HUDController : MonoBehaviour {
 
 	void Awake() {
         CanvasObject = GetComponent<Canvas>();
+		ScoreText = GameObject.Find("Score Value").GetComponent<Text>();
+		TimerText = GameObject.Find("Timer Value").GetComponent<Text>();
+		GamemodeText = GameObject.Find("Gamemode").GetComponent<Text>();
 	}
+
 	// Use this for initialization
 	void Start () {
 		
@@ -31,10 +56,33 @@ public class HUDController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-		if (Input.GetKeyDown(HideHotkey))
-        {
+		if (Input.GetKeyDown(HideHotkey)) {
             toggleView();
         }
+		if (DebugMode) {
+			HUDDebug();
+		}
+	}
+
+	/**
+	Hold F to increase score
+	Press G to set score to number of seconds remaining in the timer
+	 */
+	private void HUDDebug() {
+		if (Input.GetKey(KeyCode.F)) {
+			updateScoreValue(int.Parse(ScoreText.text) + 1);
+		}
+		if (Input.GetKeyDown(KeyCode.G)) {
+			updateTimerSecondsRemaining(int.Parse(ScoreText.text) + 1);
+		}
+		if (Input.GetKeyDown(KeyCode.H)) {
+			if (int.Parse(ScoreText.text) % 2 == 1) {
+				updateGamemodeString("ODD");
+			} else {
+				updateGamemodeString("EVEN");
+			}
+
+		}
 	}
 
 }
