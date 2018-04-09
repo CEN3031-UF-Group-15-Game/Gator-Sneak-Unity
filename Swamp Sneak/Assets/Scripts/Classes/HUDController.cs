f7c6dd1b9300f2f0e44a89e023af39a969d4a195
Jazy Llerena <jazy@jazyserver.com>
2018-04-09 01:13:50 -0400

    Create a new HUD Controller script
    Remove the old HUD functionality and move it to the new controller

---
+++ Swamp Sneak/Assets/Scripts/Classes/HUDController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HUDController : MonoBehaviour {
+	
+	public KeyCode HideHotkey;
+	
+    private Canvas CanvasObject;
+
+	// Hide the canvas
+	public void hide() {
+		CanvasObject.enabled = false;
+	}
+	// Show the canvas
+	public void show() {
+		CanvasObject.enabled = true;
+	}
+	// Toggle hud view
+	public void toggleView() {
+		CanvasObject.enabled = !CanvasObject.enabled;
+	}
+
+	void Awake() {
+        CanvasObject = GetComponent<Canvas>();
+	}
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(HideHotkey))
+        {
+            toggleView();
+        }
+	}
+
+}
